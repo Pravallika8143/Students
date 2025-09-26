f4fa4e39f77ef49f9a805e0215abd59a3f79a8fd
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-26 12:21:55 +0530

    app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import StudentTable from "./students/StudentsTable"
 function App(){
 return (
-  <div className="border border-danger border-5 m-5 p-5">
+  <div className="border border-danger border-2 m-5 p-5">
   <h1>Students Table Generation</h1>
   <StudentTable></StudentTable>
   </div>
